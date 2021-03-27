f64f4de251e1a256a7de17d738a44414b77dde93
Rifkie <rifkiroar@gmail.com>
2021-03-27 10:43:25 +0700

    mengisi layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('mahasiswa', MahasiswaController::class);
+Route::resource('mahasiswa', App\Http\Controllers\MahasiswaController::class);
 // Route::get('/', function () {
 //     return view('welcome');
 // });
