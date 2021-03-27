a2de4e425d6827646717da4709bc29a24181136b
Rifkie <rifkiroar@gmail.com>
2021-03-27 08:36:27 +0700

    modif migration

---
+++ database/seeders/MahasiswaSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MahasiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::insert([
+            'nim' => '1941720150',
+            'nama' => 'Octavia',
+            'kelas' => 'TI-2C',
+            'jurusan' => 'JTI',
+            'no_handphone' => '0812222',
+        ]);
+    }
+}
