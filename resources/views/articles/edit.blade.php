c19220222730b5a41408df0b0082f2d99de95538
Rifkie <rifkiroar@gmail.com>
2021-04-13 22:14:04 +0700

    PWL_10 Praktikum 3, membuat export data PDF

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -2,21 +2,21 @@
 
 @section('content')
 <div class="container">
-<form action="/articles/{{$article->id}}" method="post" enctype="multipart/form-data">
+<form action="/articles/{{$articles->id}}" method="post" enctype="multipart/form-data">
     @method('PUT')
 @csrf
 <div class="form-group">
 <label for="title">Judul</label>
-<input type="text" class="form-control" required="required" name="title" value="{{$article->title}}"></br>
+<input type="text" class="form-control" required="required" name="title" value="{{$articles->title}}"></br>
 </div>
 <div class="form-group">
 <label for="content">Content</label>
-<input type="text" class="form-control" required="required" name="content" value="{{$article->content}}"></br>
+<input type="text" class="form-control" required="required" name="content" value="{{$articles->content}}"></br>
 </div>
 <div class="form-group">
 <label for="image">Feature Image</label>
-<input type="file" class="form-control" required="required" name="image" value="{{$article->featured_image}}"></br>
-<img width="150px" src="{{asset('storage/'.$article->featured_image)}}">
+<input type="file" class="form-control" required="required" name="image" value="{{$articles->featured_image}}"></br>
+<img width="150px" src="{{asset('storage/'.$articles->featured_image)}}">
 </div>
 <button type="submit" class="btn btn-primary float-right">Ubah Data</button>
 </form>
